e629631f135e5e0c102b901e1083d943af034bd3
agustin cancelare <agustincancelare@gmail.com>
2025-01-10 08:48:24 -0300

    casi terminado

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v#xg+d(^l=eviey%d-dsnyewda-o1#)wp*it6(qoom)$^^xn*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
